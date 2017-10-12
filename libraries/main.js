c04884916383162e94f3ba66741eb6d4e85648ab
Pasever <pas1034ever@gmail.com>
2017-10-12 00:22:18 -0400

    a.0.3

--- libraries/main.js
+++ libraries/main.js
@@ -14,32 +14,50 @@ firebase.initializeApp(config);
 var database = firebase.database();
 //var now = moment();
 //var time = now.hour() + ':' + now.minutes() + ':' + now.seconds();
+
+//setInterval(function(){
+//    $('#firstLine').html(moment().format('hh:mm:ss A'))
+//  }, 1000);
+//  
+ setInterval(function(){ 
 $(".hours").html(moment().hours());
 $(".minutes").html(moment().minutes());
-console.log();
+$(".seconds").html(moment().seconds());
+}, 1000); 
+
+
 var count = $('#tbody tr').length;
-    console.log(count);
 
 $("#submitButton").on("click", function (event) {
 
     event.preventDefault();
 
+    var now = moment();
     var train = $("#fieldTrain").val().trim();
     var destination = $("#fieldDest").val().trim();
     var firstTrain = $("#fieldFirstTr").val().trim();
     var frequency = $("#fieldFrequency").val().trim();
+
+    //firstTrainTime = $("#fieldFirstTr").val().trim();
+
+    setInterval(function () {
     
-//    var monthsNeg = moment(date, "DD/MM/YY").diff(moment(), "months");
-//    var months = Math.abs(monthsNeg);
-//    var billed = rate * months;
+    var formated = moment(firstTrain, "hh:mm").subtract(1, "years");
+    var diff = moment().diff(moment(formated), "minutes");
+    var apart = diff % frequency;
+    var away = frequency - apart;
+    var arrival = moment().add(away, "minutes").format("hh:mm");
 
     database.ref().push({
         train: train,
         destination: destination,
         firstTrain: firstTrain,
-        frequency: frequency
+        frequency: frequency,
+        arrival: arrival,
+        away: away
                 // dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
+}, 100);
 
     console.log(train);
     console.log(destination);
@@ -58,14 +76,17 @@ database.ref().on("child_added", function (snapshot) {
 
     console.log(snapshot.val());
 
-    $("#tbody").append("<tr>");
-    $("#tbody").append("<td>" + ($('#tbody tr').length + 1) + "</td>");
-    $("#tbody").append("<td>" + snapshot.val().train + "</td>");
-    $("#tbody").append("<td>" + snapshot.val().destination + "</td>");
-    $("#tbody").append("<td>" + snapshot.val().frequency + "</td>");
-    $("#tbody").append("<td>" + snapshot.val().firstTrain + "</td>");
-    $("#tbody").append("<td>" + "" + "</td>");
-
-
+    var newTR = $("<tr>");
+    $("#tbody").append(newTR);
+    newTR.append("<td>" + $('#tbody tr').length + "</td>");
+    newTR.append("<td>" + snapshot.val().train + "</td>");
+    newTR.append("<td>" + snapshot.val().destination + "</td>");
+    newTR.append("<td>" + snapshot.val().frequency + "</td>");
+    newTR.append("<td>" + snapshot.val().arrival + "</td>");
+    newTR.append("<td>" + snapshot.val().away + "</td>");
 });
+
+
+
+
 
